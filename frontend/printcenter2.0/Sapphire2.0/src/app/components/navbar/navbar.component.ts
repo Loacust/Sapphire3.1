423bcc236bed24ce3e034335a1c5aacc8e48fbde
Loacust <97564364+Loacust@users.noreply.github.com>
2022-06-09 16:01:34 -0600

    Logout Works

--- frontend/printcenter2.0/Sapphire2.0/src/app/components/navbar/navbar.component.ts
+++ frontend/printcenter2.0/Sapphire2.0/src/app/components/navbar/navbar.component.ts
@@ -1,18 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  signInStatus:any;
+  signIn:any;
   constructor() { }
 
   ngOnInit(): void {
+    if(localStorage.getItem('currentUser') != null ){
+     this.signIn = true
+    }
+    else{
+   this.signIn = false
+    } 
+    this.signInStatus= this.signIn
+    console.log(this.signIn)
+    return this.signInStatus; 
   }
+  
+  
+
+
   homePage() {
     
     window.open("http://127.0.0.1:8080/Index.html","_blank");
   }
+
+  logout(){
+    localStorage.removeItem('currentUser');
+    location.reload();
+    
+  }
+  loginToggle(){
+    setTimeout(() =>
+      location.reload(),4500);
+      
+    
+  
+    
+  }
+  
 }
